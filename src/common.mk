4c5a41ce0b493dc7a7e66e7f68ca6f3a4d431439
Neshad D. Pathirana <112592828+neshaddp@users.noreply.github.com>
2026-07-14 13:09:16 -0400

    Add files via upload

---
+++ src/common.mk
@@ -0,0 +1,58 @@
+CXX        = g++
+CC         = gcc
+ROOTCINT   = rootcint
+
+ROOTCFLAGS =   $(shell root-config --cflags)
+ROOTLIBS   =   $(shell root-config --libs)  # does not include -lGui
+ROOTGLIBS  =   $(shell root-config --glibs) # does     include -lGui
+ROOTINC    = -I$(shell root-config --incdir)
+
+WARN_OPT   = -Wall -Wno-long-long #-pedantic 
+OPTIM_OPT  = -O0
+DEBUG_OPT  = -g
+POS_INDEP  = -fPIC
+LFLAGS     = $(DEBUG_OPT) $(POS_INDEP)
+CFLAGS     = $(LFLAGS) $(WARN_OPT) $(OPTIM_OPT)
+CCFLAGS    = -std=c++0x $(ROOTCFLAGS)
+SOFLAGS    = -shared
+
+DICTFILES  = $(wildcard *Dictionary.*)
+OBJFILES   = $(wildcard *.o)
+GCHFILES   = $(wildcard *.gch)
+
+INCDIR     = -I$(R00TLeInstall)/src/include #-I/usr/opt/nscldaq/10.2-105/include/
+LDFLAGS    = -Wl,-rpath=$(LIBDIR) #-Wl,-rpath=/usr/opt.nscldaq/10.2-105/lib/
+
+.SECONDARY: $(OBJS)
+
+.PHONY:    all clean
+all:       $(TARGET)
+clean:
+	-@rm -fv $(TARGET) $(SHLIBS) $(OBJFILES) $(DICTFILES) $(GCHFILES) 2>/dev/null || true
+
+%.o: %.c
+	@echo "Compiling $<..."
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+%.o: %.cc %.hh
+	@echo "Compiling $<..."
+	@$(CXX) $(CFLAGS) $(CCFLAGS) $(INCDIR) -c $< -o $@
+
+$(R00TLeInstall)/lib/lib%.so: $(OBJS)
+	@echo "Building Library $@..."
+	@$(CXX) $(LFLAGS) $(SPECIALLIBS) $(LDFLAGS) $(ROOTLIBS) $(SOFLAGS) -o $@ $^
+
+$(R00TLeInstall)/bin/%: %.cc
+	@echo "Building target $<..."
+	@$(CXX) $(CFLAGS) $(CCFLAGS) $(ROOTLIBS) -L$(LIBDIR) $(LIBS) $(INCDIR) $(LDFLAGS) $< -o $@
+
+%Dictionary.o: %.hh %LinkDef.h
+	@echo "Compiling ROOT dictionary $(patsubst %.o,%.cc,$@)..."
+	@$(ROOTCINT) -f $(patsubst %.o,%.cc,$@) -c -p $(INCDIR) $^;
+	@$(CXX) $(CFLAGS) $(CCFLAGS) $(INCDIR) -c $(patsubst %.o,%.cc,$@) -o $@
+
+%Dictionary3.o: %.hh $(SPECIALHEADERS) %LinkDef.h
+	@echo "Compiling ROOT dictionary $(patsubst %.o,%.cc,$@)..."
+	@$(ROOTCINT) -f $(patsubst %.o,%.cc,$@) -c -p $(INCDIR) $^;
+	@$(CXX) $(CFLAGS) $(CCFLAGS) $(INCDIR) -c $(patsubst %.o,%.cc,$@)
+
